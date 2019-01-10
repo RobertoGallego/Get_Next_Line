5f739239285fa6581ed3c6baf36816fce71a0880
Gallego ROBERTO <groberto@e3r10p4.42.fr>
2019-01-10 15:04:36 +0100

    test de git branch

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: groberto <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/27 16:01:57 by groberto          #+#    #+#             */
-/*   Updated: 2018/12/07 11:08:23 by groberto         ###   ########.fr       */
+/*   Updated: 2019/01/10 15:04:05 by groberto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@
 # include <fcntl.h>
 # include <stdlib.h>
 # include <unistd.h>
-# define BUFF_SIZE 32
+# define BUFF_SIZE 64
 
 int		get_next_line(const int fd, char **line);
 
